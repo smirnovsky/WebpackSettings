0afedfc22d7abf2925f12cb3ddd5e991b4ea133b
smirnovsky <happy280190@gmail.com>
2022-04-26 15:22:50 +0300

    HTMLPlugin
    Plugin Connection

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const { Template } = require('webpack')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HTMLPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 }
